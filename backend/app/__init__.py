08416135a0f1a5cc7248a543bfc606419473b635
pyjoek <joelnrlson@gmail.com>
2025-08-27 21:56:47 +0300

    dat 2 database setup

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://trackuser:trackpass@dv/expense_tracker"
+    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://trackuser:trackpass@db/expense_tracker"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SECRET_KEY'] = "supersecretkey"
 
---
+++ backend/app/model.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from . import db
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.column(db.Integer, primary_key = True)
+    username = db.Column(db.String(50), unique = True, nullable = False)
+    email = db.Column(db.String(120), unique = True, nullable = False)
+    password = db.Column(db.String(200), nullable = False)
+    created_at = db.Column(db.DateTime, default = datetime.utcnow)
+
+class Category(db.Model):
+    __tablename__ = "categories"
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(50), nullable = False)
+
+class Expense(db.Model):
+    __tablename__ = "expenses"
+    id = db.Column(db.Integer, primary_key = True)
+    limit = db.Column(db.Float, nullable = False)
+    month = db.Column(db.String(20), nullable = False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+class Budget(db.Model):
+    __tablename__ = "budgets"
+    id = db.Column(db.Integer, primary_key=True)
+    limit = db.Column(db.Float, nullable=False)
+    month = db.Column(db.String(20), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
